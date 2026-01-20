75df75e430700d62d3dfbcb4825e263483faa9d0
swa1245 <swarajpatil2123@gmail.com>
2026-01-20 13:28:45 +0530

    mac-os setup + dock added with backroundimage

---
+++ src/components/Dock.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./dock.scss";
+
+const Dock = () => {
+  return (
+    <footer className="dock">
+      <div className="icon github">
+        {/* <img src="/doc-icon/github-svgrepo-com.svg" alt="" /> */}
+      </div>
+      <div className="icon note">
+        {/* <img src="/doc-icon/note-svgrepo-com.svg" alt="" /> */}
+      </div>
+      <div className="icon pdf">
+        {/* <img src="/doc-icon/pdf-svgrepo-com.svg" alt="" /> */}
+      </div>
+      <div className="icon calendar">
+        {/* <img src="/doc-icon/calendar-svgrepo-com.svg" alt="" /> */}
+      </div>
+      <div className="icon spotify">
+        {/* <img src="/doc-icon/spotify-svgrepo-com.svg" alt="" /> */}
+      </div>
+      <div className="icon link">
+        {/* <img src="/doc-icon/link-svgrepo-com.svg" alt="" /> */}
+      </div>
+      <div className="icon cli">
+        {/* <img src="/doc-icon/cli-svgrepo-com.svg" alt="" /> */}
+      </div>
+    </footer>
+  );
+};
+
+export default Dock;
